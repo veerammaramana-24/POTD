6b3c9abcb22114e9e7c3b051b23962a7e1ab4486
veerammaramana-24 <veerammach24@gmail.com>
2025-03-23 23:01:10 +0530

    success

--- Total_Decoding_Messages.cpp
+++ Total_Decoding_Messages.cpp
@@ -1,4 +1,50 @@
-completed
- but naku em ardham avale
-ipudu vallu vachi explain cheyama ante em chepali
-wait
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int solve(string &digits,int n,int i,vector<int>&dp){
+        //base case
+        if(i==n) return 1;
+        if(digits[i]=='0') return 0 ;
+        //recursive case
+        if(dp[i]!=-1) return dp[i];
+        int one=solve(digits,n,i+1,dp);
+        int two=0;
+        if(i!=n-1 && (digits[i]=='1' ||digits[i]=='2' && digits[i+1]<='6' ))
+        two = solve(digits,n,i+2,dp);
+        return dp[i]= one+two;
+    }
+    int countWays(string &digits) {
+        // CodeGenius
+        
+        int n=digits.size();
+        vector<int>dp(n+1,-1);
+        return solve(digits,n,0,dp);
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
+        string digits;
+        getline(cin, digits);
+        Solution obj;
+        int ans = obj.countWays(digits);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
